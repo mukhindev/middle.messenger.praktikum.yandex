a18e5c9ff3f64817bb6b5e9ff9b8a60efd8fbb13
mukhindev <mukhin.dev@gmail.com>
2021-07-20 14:22:03 +0300

    Установить и настроить helmet для Express

--- src/server.js
+++ src/server.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
 const history = require('connect-history-api-fallback');
 const limiter = require('./serverMiddlewares/limiter');
 const { PORT } = require('./serverUtils/serverEnv');
-const { DISTRIBUTIVE_DIR } = require('./serverUtils/serverСonstants');
+const { DISTRIBUTIVE_DIR } = require('./serverUtils/serverConstants');
 
 const distributiveDirPath = path.join(__dirname, '../', DISTRIBUTIVE_DIR);
 
 const app = express();
 
 app.use(limiter);
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
+app.use(helmet.contentSecurityPolicy({
+  useDefaults: true,
+  directives: {
+    'default-src': [
+      "'self'",
+      'https://ya-praktikum.tech',
+      'wss://ya-praktikum.tech',
+      'ws://localhost:*',
+      "'unsafe-inline'",
+    ],
+    'script-src': [
+      "'self'",
+      "'sha256-vvt4KWwuNr51XfE5m+hzeNEGhiOfZzG97ccfqGsPwvE='",
+    ],
+  },
+}));
 app.use(history());
 app.use(express.static(distributiveDirPath));
 

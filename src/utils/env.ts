d269e76fdad52870391a86628e0fdf75fb5b9416
mukhindev <mukhin.dev@gmail.com>
2021-07-26 17:36:58 +0300

    Перевести проект с Parcel на Webpack

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,6 +1,6 @@
 export default {
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  HOST_API: process.env.HOST_API || 'https://ya-praktikum.tech/api/v2',
-  HOST_RESOURCES: process.env.HOST_RESOURCES || 'https://ya-praktikum.tech/api/v2/resources',
-  HOST_WS: process.env.HOST_WS || 'wss://ya-praktikum.tech/ws',
+  NODE_ENV: 'development',
+  HOST_API: 'https://ya-praktikum.tech/api/v2',
+  HOST_RESOURCES: 'https://ya-praktikum.tech/api/v2/resources',
+  HOST_WS: 'wss://ya-praktikum.tech/ws',
 };
